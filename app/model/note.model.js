1348fce0af69e1d5b5b3d378e9f8235913660110
e.savenko <savenko.jenia@gmail.com>
2019-05-27 16:35:16 +0300

    -add moment js

--- app/model/note.model.js
+++ app/model/note.model.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
+const now = moment();
 
 const NoteSchema = mongoose.Schema({
     title: String,
     text: String
 }, {
-    timestamps: false
+    timestamps: {type: String, default: now.format("dddd, MMMM Do YYYY, h:mm:ss a")}
 });
 
 module.exports = mongoose.model('Note', NoteSchema);
--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
     url : 'mongodb+srv://admin:2mn12d61409@cluster0-qsuzq.mongodb.net/testDataBase?retryWrites=true'
+
 };
