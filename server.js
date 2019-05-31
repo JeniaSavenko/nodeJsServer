493ad7a3e1c2d6b10358a8efc219cc57a64bdfd6
e.savenko <savenko.jenia@gmail.com>
2019-05-31 18:50:48 +0300

    add socket.io(work in progress)

--- server.js
+++ server.js
@@ -1,73 +1,37 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-const db = require('./config/db');
-const app = express();
-const port = 8000;
-const cors = require('cors');
-const socketPort = 3000
-const mongoose = require('mongoose');
-const socket = require('socket.io');
+const Koa = require('koa');
+const IO = require('koa-socket');
 
-var server = app.listen(5000,()=>{
-    console.log("Howdy, I am running at PORT 5000")
-})
+const app = new Koa();
+const io = new IO();
 
+io.attach(app);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
+io.on('connection', ctx => {
+    console.log('[server] connected');
 });
 
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(bodyParser.json());
-app.use(cors());
-
-require('./app/routes/index.js')(app);
-
-
-
-/*MongoClient.connect(db.url, (err) => {
-    if (err) return console.log(err);
-    require('./app/routes/index.js')(app);
-    app.listen(port, () => {
-        console.log('We are live on ' + port);
-    });
-})*/
-
-mongoose.connect(db.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
+let usernames = [];
+io.on('disconnect', ctx => {
+    const { username } = ctx.socket;
+    if (username) {
+        console.log(`[server] disconnected: ${username}`);
+        usernames = usernames.filter(u => u !== username)
+    }
 });
 
-mongoose.connection.on('error',()=>{
-    console.log("Error in database connection")
-})
-mongoose.connection.once('open',function(){
-    console.log("DB connection established")
-})
+let messages = [];
+io.on('CREATE_POST', (ctx, { text }) => {
+    console.log(`[server] message: ${text}`);
+    const message = {
+        title,
+        text,
+    };
+    messages.push(message);
+    console.log(message);
 
+    io.broadcast('CREATE_POST', { message });
+});
 
-let io =  socket(server);
-
-
-
-
-io.on("connection", function(socket){
-    console.log("Socket Connection Established with ID :"+ socket.id)
-    socket.on("createPost", async function(postCreated){
-        let response = await postCreated
-        console.log(response);
-        socket.emit("postCreated",postCreated)
-    })
-})
-
-
+app.listen(3000, () => {
+    console.log('[server] ready');
+});
